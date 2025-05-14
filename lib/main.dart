2ae4fbd7e764051c392c8eede222e0000c85edee
JAN_ADRIAN_SOSA_CORDERO <a.22308051281317@cbtis128.edu.mx>
2025-05-14 19:55:16 +0000

    act8

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,225 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ));
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class HomePage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _HomePageState createState() => _HomePageState();
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class _HomePageState extends State<HomePage> {
+  final List<List<String>> products = [
+    ['assets/images/watch-1.jpg', 'Hugo Boss Oxygen', '100 \$'],
+    ['assets/images/watch-2.jpg', 'Hugo Boss Signature', '120 \$'],
+    ['assets/images/watch-3.jpg', 'Casio G-Shock Premium', '80 \$']
+  ];
+
+  int currentIndex = 0;
+
+  void _next() {
+    setState(() {
+      if (currentIndex < products.length - 1) {
+        currentIndex++;
+      } else {
+        currentIndex = currentIndex;
+      }
+    });
+  }
+
+  void _preve() {
+    setState(() {
+      if (currentIndex > 0) {
+        currentIndex--;
+      } else {
+        currentIndex = 0;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      body: Container(
+        color: Colors.white,
+        child: Column(
+          children: <Widget>[
+            GestureDetector(
+              onHorizontalDragEnd: (DragEndDetails details) {
+                if (details.velocity.pixelsPerSecond.dx > 0) {
+                  _preve();
+                } else if (details.velocity.pixelsPerSecond.dx < 0) {
+                  _next();
+                }
+              },
+              child: AnimatedOpacity(
+                duration: Duration(milliseconds: 500),
+                opacity: 1.0,
+                child: Container(
+                  width: double.infinity,
+                  height: 600,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage(products[currentIndex][0]),
+                          fit: BoxFit.cover)),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            begin: Alignment.bottomRight,
+                            colors: [
+                          Colors.grey.shade700.withOpacity(.9),
+                          Colors.grey.withOpacity(.0),
+                        ])),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.only(bottom: 60),
+                          child: Container(
+                            width: 90,
+                            child: Row(
+                              children: _buildIndicator(),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Transform.translate(
+                offset: Offset(0, -40),
+                child: Padding(
+                  padding: EdgeInsets.only(top: 30),
+                  child: Container(
+                    width: double.infinity,
+                    padding: EdgeInsets.all(30),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(30),
+                            topRight: Radius.circular(30))),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Column(
+                          children: [
+                            Text(
+                              products[currentIndex][1],
+                              style: TextStyle(
+                                  color:
+                                      Colors.black, // Cambiado a Colors.black
+                                  fontSize: 40,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              height: 15,
+                            ),
+                            Row(
+                              children: <Widget>[
+                                Text(
+                                  products[currentIndex][2],
+                                  style: TextStyle(
+                                      color: Colors.yellow[700],
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20),
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Row(
+                                  children: <Widget>[
+                                    Icon(
+                                      Icons.star,
+                                      size: 18,
+                                      color: Colors.yellow[700],
+                                    ),
+                                    Icon(
+                                      Icons.star,
+                                      size: 18,
+                                      color: Colors.yellow[700],
+                                    ),
+                                    Icon(
+                                      Icons.star,
+                                      size: 18,
+                                      color: Colors.yellow[700],
+                                    ),
+                                    Icon(
+                                      Icons.star,
+                                      size: 18,
+                                      color: Colors.yellow[700],
+                                    ),
+                                    Icon(
+                                      Icons.star_half,
+                                      size: 18,
+                                      color: Colors.yellow[700],
+                                    ),
+                                    SizedBox(
+                                      width: 5,
+                                    ),
+                                    Text(
+                                      "(4.2/70 reviews)",
+                                      style: TextStyle(
+                                          color: Colors.grey, fontSize: 12),
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                        MaterialButton(
+                          onPressed: () {},
+                          height: 45,
+                          color: Colors.yellow[700],
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8)),
+                          child: Center(
+                            child: Text(
+                              "ADD TO CART",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
+  }
+
+  Widget _indicator(bool isActive) {
+    return Expanded(
+      child: Container(
+        height: 4,
+        margin: EdgeInsets.only(right: 5),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(50),
+            color: isActive ? Colors.grey[800] : Colors.white),
+      ),
+    );
+  }
+
+  List<Widget> _buildIndicator() {
+    List<Widget> indicators = [];
+    for (int i = 0; i < products.length; i++) {
+      if (currentIndex == i) {
+        indicators.add(_indicator(true));
+      } else {
+        indicators.add(_indicator(false));
+      }
+    }
+
+    return indicators;
   }
 }
